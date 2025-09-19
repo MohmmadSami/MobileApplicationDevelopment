3eb3f65a01ef69f300943105a8567be2f3530d5e
Samiullah Khiljie <s2087434@gmail.com>
2025-09-18 21:39:56 -0700

    update task

---
+++ lib/quize/question01.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Counter App',
+      home: CounterScreen(),
+    );
+  }
+}
+
+// Using StatefulWidget to manage state
+class CounterScreen extends StatefulWidget {
+  @override
+  _CounterScreenState createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int counter = 0; // initial value
+
+  // function to increase counter by 10
+  void _addTen() {
+    setState(() {
+      counter += 10;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Counter Example")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Counter Value:", style: TextStyle(fontSize: 22)),
+            SizedBox(height: 10),
+            Text(
+              "$counter",
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _addTen,
+              child: Text("Add 10"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
